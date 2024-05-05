77c2300bccfd3a3093281ea5680ab4a37cf82f08
drccastillo <darwin.castillo.jala@gmail.com>
2024-05-05 16:39:48 -0400

    Update run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -7,4 +7,4 @@ source .venv/bin/activate
 python3 -m pytest booker_api/rooms/test_rooms.py booker_api/bookings/test_bookings.py booker_api/brandings/test_brandings.py booker_api/messages/test_messages.py booker_api/reports/test_reports.py -v -s --alluredir reports/allure/results
 
 # Generate Allure report
-allure serve --port 9999 reports/allure/reports
+allure serve --port 9999 reports/allure/results
